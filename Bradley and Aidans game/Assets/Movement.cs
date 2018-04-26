66e13b4f137785503b5c7c059da3b18618c82f26
Nernd <Nernd25555@gmail.com>
2018-04-26 08:03:36 -0700

    hey

---
+++ Bradley and Aidans game/Assets/Movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+	public Rigidbody rb;
+
+	public float sidewaysForce = 30f;
+
+
+	void FixedUpdate () {
+
+
+		if (Input.GetKey("d"))
+		{
+
+			rb.AddForce (sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);	
+		}
+		if (Input.GetKey("a"))
+		{
+			rb.AddForce (-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);	
+		}
+		if (Input.GetKey("w"))
+		{
+
+			rb.AddForce (0, 0, sidewaysForce * Time.deltaTime ,ForceMode.VelocityChange);	
+		}if (Input.GetKey("s"))
+		{
+			rb.AddForce (0, 0, -sidewaysForce * Time.deltaTime, ForceMode.VelocityChange);	
+		}
+
+	} 
+}
